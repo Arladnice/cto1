e3527a8c6895662651ddca5cbca0162204f212bc
Denis Arlakov <darlakov@mail.ru>
2025-11-12 19:40:10 +0300

    Remove

--- README.md
+++
@@ -1,9 +0,0 @@
-# QCM.Front
-
-### Установка зависимостей
-```bash
-  npm i sass -g
-  npm i
-```
-### Настройка SCSS-препроцессора
-![settings_scss.png](docs/settings_scss.png)
--- jest.setup.ts
+++
@@ -1,9 +0,0 @@
-import '@testing-library/jest-dom';
-
-jest.mock('@/lib/events.ts', () => ({
-    eventBus: {
-        addEventListener: jest.fn(),
-        removeEventListener: jest.fn(),
-        dispatchEvent: jest.fn(),
-    },
-}));
